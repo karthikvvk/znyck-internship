d972b03b9b14e53dd0fdb74d3f927aea14a6cce4
karthik <vkarthik5002@gmail.com>
2024-09-25 18:58:02 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,9 +4,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="font-Poppins min-h-screen flex flex-col bg-gradient-to-r from-[#566FFD] to-[#F866C6]">
-      <Head>
-        <title>The Social Project</title>
-      </Head>
+      <div  className="bg-gradient-to-r from-[#566FFD] to-[#F866C6]">
+
 
       <nav className="flex justify-between items-center border border-gray-700">
         <div className="flex space-x-14 p-6 ">
@@ -35,7 +34,7 @@ export default function Home() {
           <input className='width-15 rounded-s py-3'></input>
           <button className="bg-purple-600 text-white px-6 py-3 rounded-l-none rounded-sm">Join us today</button>
         </div>
-        
+  
         <div className='flex justify-center md:justify-end md:w-1/2 p-4'>
           <Image
             src="/child.png"
@@ -45,6 +44,41 @@ export default function Home() {
           />
         </div>
       </div>
+      </div>
+
+      <div className=" text-[#000000] font-Poppins min-h-screen flex flex-col bg-[#DFDDE0]">
+        <header className='font-Poppins flex flex-col'>
+          <h1 className='text text-[28px]  font-bold text-[#000000] md:text-center mt-10'>EMPOWERING COMMUNITIES: OVERCOMING THE CYCLE OF POVERTY AND INEQUALITY</h1>
+        </header>
+        <div className='flex flex-col text text-[18px] p-10'>
+            <p>
+            In a world where poverty and inequality continue to hinder progress for millions, countless individuals and families are left without
+              the resources they need to thrive. At The Social Project, we are committed to changing this narrative by empowering the most 
+              vulnerable members of our society—including children, youth, women, and marginalized communities—through education, mentorship,
+              health camps, and social initiatives.
+            </p>
+            <p className='mt-7'>Our mission is to provide essential support across various areas:</p>
+            <ul className='list-disc'>
+              <div className='ml-9'>          
+                <li className='mt-7'>Education: Offering quality education and life skills to children and youth, ensuring they have the tools to build a brighter future.</li>
+                <li className='mt-7'>Women Empowerment: Empowering women through skills training and support programs, helping them achieve financial independence and improved quality of life.</li>
+                <li className='mt-7'>Health and Awareness: Conducting health camps, awareness programs, and environmental initiatives that promote well-being and sustainable living.</li></div>
+            </ul>
+            <p className='mt-7'>Community Support: Collaborating with local organizations and social groups to foster growth and resilience within communities.</p>
+
+            <p className='mt-7'>
+              Join us in our mission to uplift and empower communities across our reach. Your support is crucial in expanding our initiatives and ensuring that every individual has access to the opportunities they deserve.
+            </p>
+            <p>
+            Together, we can make a lasting impact—transforming lives and creating a world where everyone has the chance to succeed. Let’s work together to build a better, brighter future for all.
+            </p>
+          </div>
+          <div className=' justify-center items-center flex flex-row'>
+            <button className="bg-[#17D8E2] text-[#0F0F0F] px-4 py-2 rounded-full">Donate</button>
+          </div>
+      </div>
+
     </div>
+    
   )
 }
